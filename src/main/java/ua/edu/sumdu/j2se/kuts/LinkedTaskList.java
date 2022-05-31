7a69daab178219687704a3a45c835c01381dc652
Inna Kuts <innakutss02@gmail.com>
2022-05-31 21:29:19 +0300

    LinkedTaskList is done

--- src/main/java/ua/edu/sumdu/j2se/kuts/LinkedTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/kuts/LinkedTaskList.java
@@ -1,58 +1,83 @@
 package ua.edu.sumdu.j2se.kuts;
 
-import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class LinkedTaskList {
 
     private Node head;
-    private int data;
-    private int indexTracker = 0;
-    private Task[] taskList = new Task[indexTracker];
+    private Node tail;
+    private int amountOfElements = 0;
 
-    public LinkedTaskList() {
-    }
     public void add(Task task) {
-        Node newNode = new Node(data);
-        if (this.head == null) {
-            head = newNode;
-            if (indexTracker > taskList.length) {
-                taskList[indexTracker++] = task;
-            }
-        } else {
-            Node currentNode = head;
-            while (currentNode.getNextNode() != null) {
-                currentNode = currentNode.getNextNode();
-            }
-            currentNode.setNextNode(newNode);
+        if (task == null) {
+            return;
         }
+        Node oldTail = tail;
+        Node newNode = new Node(task);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail = newNode;
+            oldTail.setNextNode(newNode);
+        }
+        amountOfElements++;
+
+
     }
 
     public boolean remove(Task task) {
-        Node node = head;
-        for (int i = 0; i < indexTracker; i++) {
-            node = node.getNextNode();
+      if (head == null) {
+          throw new NoSuchElementException("Element does not exist!");
+      }
+        Node current = head;
+        Node previous = head;
+        while (current != null && !task.equals(current.getData())) {
+            previous = current;
+            current = current.getNextNode();
         }
-        node.setNextNode(node.getNextNode());
+        if (current != null) {
+            if (current == head) {
+                head = current.getNextNode();
+            } else if (current == tail) {
+                tail = previous;
+                tail.setNextNode(null);
+            } else {
+                previous.setNextNode(current.getNextNode());
+            }
+            previous.setNextNode(current.getNextNode());
+            amountOfElements--;
+        return true;
+        }
         return false;
     }
 
     public int size() {
-        return indexTracker;
+        return amountOfElements;
     }
 
-    public Task getTask(int index) throws IndexOutOfBoundsException {
+    public Task getTask(int index) {
         if (index >= size()) {
             throw new IndexOutOfBoundsException("Out of valid range");
         }
-        return new Task("", 0);
+        int i = 0;
+        Node result = head;
+        while (i != index) {
+            result = result.getNextNode();
+            i++;
+        }
+        return result.getData();
     }
 
     public LinkedTaskList incoming(int from, int to) {
         LinkedTaskList result = new LinkedTaskList();
-        for (Task task : taskList) {
-            if (task.getTime() > from && task.getTime() <= to) {
-                result.add(task);
+        Node currentEl = head;
+        while (currentEl != null) {
+            if (currentEl.data.getTime() > from && currentEl.data.getTime() <= to && currentEl.data.isActive()) {
+                result.add(currentEl.data);
             }
+            currentEl = currentEl.getNextNode();
         }
         return result;
     }
@@ -62,11 +87,38 @@ public class LinkedTaskList {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         LinkedTaskList that = (LinkedTaskList) o;
-        return Arrays.equals(taskList, that.taskList);
+        return amountOfElements == that.amountOfElements;
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(taskList);
+        return Objects.hash(amountOfElements);
+    }
+
+    private class Node {
+        private Task data;
+        private Node nextNode;
+
+        public Node(Task data) {
+            this.data = data;
+        }
+
+        public Task getData() {
+            return data;
+        }
+
+        public void setData(Task data) {
+            this.data = data;
+        }
+
+        public Node getNextNode() {
+            return nextNode;
+        }
+
+        public void setNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
     }
 }
+
+
