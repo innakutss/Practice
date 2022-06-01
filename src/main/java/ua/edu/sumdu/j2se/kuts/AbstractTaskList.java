fbbfa26072881b60666d532aae37f65b7ab4d871
Inna Kuts <innakutss02@gmail.com>
2022-06-01 11:02:30 +0300

    Practice5 created

---
+++ src/main/java/ua/edu/sumdu/j2se/kuts/AbstractTaskList.java
@@ -0,0 +1,10 @@
+package ua.edu.sumdu.j2se.kuts;
+
+public abstract class AbstractTaskList {
+
+    public abstract void add();
+
+    public abstract boolean remove();
+
+    public abstract AbstractTaskList incoming();
+}
